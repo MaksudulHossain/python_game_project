1e8c20f7357e84c3aeb39445539f8ddc2b82476f
MaksudulHossain <jewelmaksud@gmail.com>
2025-07-13 08:31:28 -0700

    settings object

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,18 @@
 import sys 
 import pygame
+from settings import Settings
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((800,600))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
         #set bg 
-        self.bg_color = (230,230,230)
+        self.bg_color = Settings
 
 
 
@@ -22,7 +24,7 @@ class AlienInvasion:
                     sys.exit() 
 
             # redraw 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
